2bd4a116aaa42d3e01e804ceb8914be852d7f37e
woeltjen <woeltjen@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2013-01-01 20:59:07 +0000

    Web demo of Xokoban cross-compiled with Emscripten; missing links/notes

---
+++ branches/sdl/doc/homepage/emscripten/index.php
@@ -0,0 +1,13 @@
+<?php
+  $site_title = "Web Demo";
+?>
+<?php
+  include("../header.html");
+  include("../content_top.html") ;
+?>
+<h1>Web Demo</h1>
+<canvas id="canvas" oncontextmenu="event.preventDefault()" style="padding-right: 0; margin-left: auto; margin-right: auto; display: block; border: 1px black solid; z-index: 10;"></canvas>
+<script type="text/javascript" src="xoEmscF2xpnML.js"/></script>
+<?php
+  include("../footer.html");
+?>

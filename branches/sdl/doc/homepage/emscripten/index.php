0772a474e76dc7a723302ff2935c5468da8af00c
woeltjen <woeltjen@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2013-01-04 06:03:21 +0000

    Added text/links to Xokoban web demo

--- branches/sdl/doc/homepage/emscripten/index.php
+++ branches/sdl/doc/homepage/emscripten/index.php
@@ -5,8 +5,28 @@
   include("../header.html");
   include("../content_top.html") ;
 ?>
-<h1>Web Demo</h1>
+<h1>Android to HTML5 - Xokoban Web Demo</h1>
+<p>
 <canvas id="canvas" oncontextmenu="event.preventDefault()" style="padding-right: 0; margin-left: auto; margin-right: auto; display: block; border: 1px black solid; z-index: 10;"></canvas>
+</p>
+<p>This pages features an Android application that was cross-compiled to
+HTML5 using XMLVM and <a href = "https://github.com/kripken/emscripten">emscripten</a>. XMLVM was used to cross-compile the
+Android app to C code using Posix API and emscripten generated the HTML5.</p>
+
+<p>Android app:
+<a href = "https://play.google.com/store/apps/details?id=org.xmlvm.demo.xokoban">
+https://play.google.com/store/apps/details?id=org.xmlvm.demo.xokoban</a></p>
+
+<p>Statistics of the cross-compiled version:</p>
+<ul>
+<li>Size: 3.5 HTML/JS + 1.5 MB graphic assets</li>
+<li>Number of JRE classes: 244</li>
+<li>Number of Android classes: 100</li>
+<li>Number of XMLVM classes: 61</li>
+<li>Number of application classes: 33</li>
+<li>Total classes: 438</li>
+</ul>
+<p>This work was funded by a grant from the Mozilla Foundation.</p>
 <script type="text/javascript" src="xoEmscF2xpnML.js"/></script>
 <?php
   include("../footer.html");

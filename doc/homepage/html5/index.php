b06fd7ee2e3880cbb552ef0ad2465496736b969d
apuder <apuder@0874c2aa-48f6-4a0f-8a6b-e32f4465a257>
2013-01-07 22:30:43 +0000

    Fix some typos.

--- doc/homepage/html5/index.php
+++ doc/homepage/html5/index.php
@@ -13,9 +13,9 @@
 </div>
 <canvas id="canvas" oncontextmenu="event.preventDefault()" style="padding-right: 0; margin-left: auto; margin-right: auto; display: block; border: 1px black solid;"></canvas>
 </p>
-<p>This pages features an Android application that was cross-compiled to
+<p>This page features an Android application that was cross-compiled to
 HTML5 using XMLVM and <a href = "https://github.com/kripken/emscripten">emscripten</a>. XMLVM was used to cross-compile the
-Android app to C code using Posix API and emscripten generated the HTML5.</p>
+Android app to C code using Posix/SDL API and emscripten generated the HTML5.</p>
 
 <p>Android app:
 <a href = "https://play.google.com/store/apps/details?id=org.xmlvm.demo.xokoban">
@@ -23,7 +23,7 @@ https://play.google.com/store/apps/details?id=org.xmlvm.demo.xokoban</a></p>
 
 <p>Statistics of the cross-compiled version:</p>
 <ul>
-<li>Size: 3.5 HTML/JS + 1.5 MB graphic assets</li>
+<li>Size: 3.5 MB HTML/JS + 1.5 MB graphic assets</li>
 <li>Number of JRE classes: 244</li>
 <li>Number of Android classes: 100</li>
 <li>Number of XMLVM classes: 61</li>
